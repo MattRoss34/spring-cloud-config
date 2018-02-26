8c7821116b7101a683679cc47583f9f9b98e0f3e
Matt Ross <matthewtross@yahoo.com>
2018-02-25 21:55:01 -0600

    Remove rogue script

--- test/example-usage.js
+++
@@ -1,6 +0,0 @@
-const springCloudConfig = require('../index');
-const configOptions = {
-    configPath: __dirname + '/config',
-    activeProfiles: ['dev']
-};
-const myConfig = springCloudConfig.load(configOptions);
