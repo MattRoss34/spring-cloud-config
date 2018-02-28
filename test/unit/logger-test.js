bf07b50ca5864e46f9ed1781babce152c440d079
Matt Ross <matthewtross@yahoo.com>
2018-02-27 22:30:04 -0600

    Add assertion to logger test

--- test/unit/logger-test.js
+++ test/unit/logger-test.js
@@ -8,6 +8,7 @@ describe('logger', function() {
             return new Promise((resolve, reject) => {
                 logger.info();
                 logger.info("test", { aGoodTest: true });
+                assert.isOk("Success", "This test was successful");
                 resolve();
             });
         });
