cbc80bb7c7a775b3166ebeeebeb3d7c9c67921fd
Matt Ross <matthewtross@yahoo.com>
2018-11-13 01:04:11 -0600

    Upgrade Dependencies

--- test/unit/logger-test.js
+++ test/unit/logger-test.js
@@ -6,7 +6,7 @@ describe('logger', function() {
 	describe("#config", function () {
 		it("should configure logger", function () {
             return new Promise((resolve, reject) => {
-                logger.info();
+                logger.info("");
                 logger.info("test", { aGoodTest: true });
                 assert.isOk("Success", "This test was successful");
                 resolve();
