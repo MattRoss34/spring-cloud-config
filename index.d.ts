02e4dddead00e5f8474b85cc783fd8134dd6ec77
Matt Ross <matthewtross@yahoo.com>
2019-06-14 01:39:06 -0500

    Add Promise to return type

--- index.d.ts
+++ index.d.ts
@@ -9,7 +9,7 @@ export interface CloudConfig {
     [name: string]: any
 }
 
-export function load(options: CloudConfigOptions): CloudConfig;
+export function load(options: CloudConfigOptions): Promise<CloudConfig>;
 
 export function instance(): CloudConfig;
 
