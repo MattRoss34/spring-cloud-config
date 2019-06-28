508dba2f9b03e8ebb909543d13947fbe7c50e11c
Matt Ross <matt.ross@asurion.com>
2019-06-28 10:32:13 -0500

    Switch to function exports instead of class

--- src/SpringCloudConfig.ts
+++ src/SpringCloudConfig.ts
@@ -6,147 +6,138 @@ import logger from './logger';
 import { ConfigObject, CloudConfigOptions } from './models';
 import { mergeProperties, readYamlAsDocument, parsePropertiesToObjects } from './utils';
 
-export class SpringCloudConfig {
-	private bootstrapConfig: ConfigObject;
-	private config: ConfigObject;
+let bootstrapConfig: ConfigObject;
+let config: ConfigObject;
 
-	public constructor() {}
-
-	/**
-	 * Initialize the config instance by reading all property sources.
-	 * 
-	 * @param {CloudConfigOptions} options The config options that drive behavior here.
-	 * @returns {ConfigObject} The merged configuration properties from all sources.
-	 */
-	public async load(options: CloudConfigOptions): Promise<ConfigObject> {
-		// options.bootstrapPath is optional
-		if (!(options.configPath && options.activeProfiles))
-			throw new Error("Invalid options supplied. Please consult the documentation.");
-		
-		logger.level = (options.level ? options.level : 'info');
+/**
+ * Initialize the config instance by reading all property sources.
+ * 
+ * @param {CloudConfigOptions} options The config options that drive behavior here.
+ * @returns {ConfigObject} The merged configuration properties from all sources.
+ */
+export const load = async (options: CloudConfigOptions): Promise<ConfigObject> => {
+	if (!(options.configPath && options.activeProfiles)) // options.bootstrapPath is optional
+		throw new Error("Invalid options supplied. Please consult the documentation.");
 	
-		return this.readConfig(options);
-	};
+	logger.level = (options.level ? options.level : 'info');
+
+	return readConfig(options);
+};
+
+export const instance = (): ConfigObject => {
+	return config;
+};
+
+/**
+ * Reads an application's configuration properties from various sources 
+ * and merges them into a single configuration object.
+ * 
+ * @param {CloudConfigOptions} options The config options that drive behavior here.
+ * @returns {ConfigObject} The merged configuration properties from all sources.
+ */
+export const readConfig = async (options: CloudConfigOptions): Promise<ConfigObject> => {
+	let propertiesObjects: ConfigObject[] = [];
+
+	bootstrapConfig = await readBootstrapConfig(options);
+	logger.debug("Using Bootstrap Config: " + JSON.stringify(bootstrapConfig));
+
+	const applicationConfig: ConfigObject = 
+		await readApplicationConfig(options.configPath, options.activeProfiles);
+	logger.debug("Using Application Config: " + JSON.stringify(applicationConfig));
+	propertiesObjects.push(applicationConfig);
+
+	// Override appliction name in bootstrap config if defined in application config
+	if (applicationConfig.spring 
+			&& applicationConfig.spring.cloud 
+			&& applicationConfig.spring.cloud.config 
+			&& applicationConfig.spring.cloud.config.name)
+			bootstrapConfig.spring.cloud.config.name = applicationConfig.spring.cloud.config.name;
 	
-	public instance(): ConfigObject {
-		return this.config;
-	};
+	const cloudConfig: ConfigObject = await readCloudConfig(bootstrapConfig);
+	propertiesObjects.push(cloudConfig);
 
-	/**
-	 * Reads an application's configuration properties from various sources 
-	 * and merges them into a single configuration object.
-	 * 
-	 * @param {CloudConfigOptions} options The config options that drive behavior here.
-	 * @returns {ConfigObject} The merged configuration properties from all sources.
-	 */
-	public async readConfig(options: CloudConfigOptions): Promise<ConfigObject> {
-		let propertiesObjects: ConfigObject[] = [];
+	// Bootstrap properties have the highest priority, so pushing this last
+	propertiesObjects.push(bootstrapConfig);
 
-		this.bootstrapConfig = await this.readBootstrapConfig(options);
-		logger.debug("Using Bootstrap Config: " + JSON.stringify(this.bootstrapConfig));
+	// Merge the properties into a single object
+	config = mergeProperties(propertiesObjects);
 
-		const applicationConfig: ConfigObject = 
-			await this.readApplicationConfig(options.configPath, options.activeProfiles);
-		logger.debug("Using Application Config: " + JSON.stringify(applicationConfig));
-		propertiesObjects.push(applicationConfig);
+	logger.debug('Using Config: ' + JSON.stringify(config));
+	return config
+}
 
-		// Override appliction name in bootstrap config if defined in application config
-		if (applicationConfig.spring 
-				&& applicationConfig.spring.cloud 
-				&& applicationConfig.spring.cloud.config 
-				&& applicationConfig.spring.cloud.config.name)
-				this.bootstrapConfig.spring.cloud.config.name = applicationConfig.spring.cloud.config.name;
-		
-		const cloudConfig: ConfigObject = await this.readCloudConfig(this.bootstrapConfig);
-		propertiesObjects.push(cloudConfig);
+/**
+ * Reads the application's bootstrap configuration file into an object.
+ * 
+ * @param {CloudConfigOptions} options The config options that drive behavior here.
+ * @returns {Promise<ConfigObject>} The bootstrap configuration.
+ */
+const readBootstrapConfig = async (options: CloudConfigOptions): Promise<ConfigObject> => {
+	// Load bootstrap.yml based on the profile name (like devEast or stagingEast)
+	let theBootstrapPath: string = options.bootstrapPath ? options.bootstrapPath : options.configPath;
+	const thisBootstrapConfig: ConfigObject = 
+		await readYamlAsDocument(`${theBootstrapPath}/bootstrap.yml`, options.activeProfiles);
+	thisBootstrapConfig.spring.cloud.config.profiles = options.activeProfiles;
 
-		// Bootstrap properties have the highest priority, so pushing this last
-		propertiesObjects.push(this.bootstrapConfig);
+	return thisBootstrapConfig;
+}
 
-		// Merge the properties into a single object
-		this.config = mergeProperties(propertiesObjects);
-
-		logger.debug('Using Config: ' + JSON.stringify(this.config));
-		return this.config
-	}
-
-	/**
-	 * Reads the application's bootstrap configuration file into an object.
-	 * 
-	 * @param {CloudConfigOptions} options The config options that drive behavior here.
-	 * @returns {Promise<ConfigObject>} The bootstrap configuration.
-	 */
-	public async readBootstrapConfig(options: CloudConfigOptions): Promise<ConfigObject> {
-		// Load bootstrap.yml based on the profile name (like devEast or stagingEast)
-		let theBootstrapPath: string = options.bootstrapPath ? options.bootstrapPath : options.configPath;
-		const thisBootstrapConfig: ConfigObject = 
-			await readYamlAsDocument(`${theBootstrapPath}/bootstrap.yml`, options.activeProfiles);
-		thisBootstrapConfig.spring.cloud.config.profiles = options.activeProfiles;
-
-		return thisBootstrapConfig;
-	}
-
-	/**
-	 * Read the application's configuration files and merge them into a single object.
-	 * 
-	 * @param {string} appConfigPath Path to where the application yaml files can be found.
-	 * @param {string[]} activeProfiles The active profiles to use for filtering config files.
-	 * @returns {Promise<ConfigObject>} The merged (local) application configuration files.
-	 */
-	public async readApplicationConfig(appConfigPath: string, activeProfiles: string[]): Promise<ConfigObject> {
-		const applicationConfig: ConfigObject = await readYamlAsDocument(appConfigPath + '/application.yml', activeProfiles);
-		let appConfigs: ConfigObject[] = [ applicationConfig ];
-		activeProfiles.forEach(function(activeProfile) {
-			const profileSpecificYaml = 'application-' + activeProfile + '.yml';
-			const profileSpecificYamlPath = appConfigPath + '/' + profileSpecificYaml;
-			if (fs.existsSync(profileSpecificYamlPath)) {
-				try {
-					const propDoc = yaml.safeLoad(fs.readFileSync(profileSpecificYamlPath, 'utf8'));
-					const thisDoc = parsePropertiesToObjects(propDoc);
-					appConfigs.push(thisDoc);
-				} catch (error) {
-					logger.error('Error reading profile-specific yaml: ' + error.message);
-				}
-			} else {
-				logger.debug('Profile-specific yaml not found: ' + profileSpecificYaml);
-			}
-		});
-		
-		return mergeProperties(appConfigs);
-	}
-	
-	/**
-	 * Reads the external configuration from Spring Cloud Config Server
-	 *
-	 * @param {ConfigObject} bootStrapConfig The bootstrap properties needed for Spring Cloud Config
-	 * @returns {Promise<ConfigObject>} The Spring Environment Object obtained from the Config Server
-	 */
-	public async readCloudConfig(bootStrapConfig: ConfigObject): Promise<ConfigObject> {
-		let cloudConfig = {};
-		if (bootStrapConfig.spring.cloud.config.enabled) {
-			try {
-				logger.debug("Spring Cloud Options: " + JSON.stringify(bootStrapConfig.spring.cloud.config));
-				try {
-					const cloudConfigProperties: ConfigObject = 
-						await cloudConfigClient.load(bootStrapConfig.spring.cloud.config, null);
-					if (cloudConfigProperties) {
-						cloudConfigProperties.forEach(function(key, value) {
-							cloudConfig[key] = value;
-						}, false);
-						cloudConfig = parsePropertiesToObjects(cloudConfig);
-					}
-					logger.debug("Cloud Config: " + JSON.stringify(cloudConfig));
-					return cloudConfig;
-				} catch (error) {
-					logger.error("Error reading cloud config: %s", error.message);
-					return cloudConfig;
-				};
-			} catch (e) {
-				logger.error("Caught error from cloud config client: %s", e.message);
-				return cloudConfig;
-			}
+/**
+ * Read the application's configuration files and merge them into a single object.
+ * 
+ * @param {string} appConfigPath Path to where the application yaml files can be found.
+ * @param {string[]} activeProfiles The active profiles to use for filtering config files.
+ * @returns {Promise<ConfigObject>} The merged (local) application configuration files.
+ */
+export const readApplicationConfig = async (appConfigPath: string, activeProfiles: string[]): Promise<ConfigObject> => {
+	const applicationConfig: ConfigObject = await readYamlAsDocument(appConfigPath + '/application.yml', activeProfiles);
+	let appConfigs: ConfigObject[] = [ applicationConfig ];
+	activeProfiles.forEach(function(activeProfile) {
+		const profileSpecificYaml = 'application-' + activeProfile + '.yml';
+		const profileSpecificYamlPath = appConfigPath + '/' + profileSpecificYaml;
+		if (fs.existsSync(profileSpecificYamlPath)) {
+			const propDoc = yaml.safeLoad(fs.readFileSync(profileSpecificYamlPath, 'utf8'));
+			const thisDoc = parsePropertiesToObjects(propDoc);
+			appConfigs.push(thisDoc);
 		} else {
+			logger.debug('Profile-specific yaml not found: ' + profileSpecificYaml);
+		}
+	});
+	
+	return mergeProperties(appConfigs);
+}
+
+/**
+ * Reads the external configuration from Spring Cloud Config Server
+ *
+ * @param {ConfigObject} bootStrapConfig The bootstrap properties needed for Spring Cloud Config
+ * @returns {Promise<ConfigObject>} The Spring Environment Object obtained from the Config Server
+ */
+export const readCloudConfig = async (bootStrapConfig: ConfigObject): Promise<ConfigObject> => {
+	let cloudConfig = {};
+	if (bootStrapConfig.spring.cloud.config.enabled) {
+		try {
+			logger.debug("Spring Cloud Options: " + JSON.stringify(bootStrapConfig.spring.cloud.config));
+			try {
+				const cloudConfigProperties: ConfigObject = 
+					await cloudConfigClient.load(bootStrapConfig.spring.cloud.config, null);
+				if (cloudConfigProperties) {
+					cloudConfigProperties.forEach(function(key, value) {
+						cloudConfig[key] = value;
+					}, false);
+					cloudConfig = parsePropertiesToObjects(cloudConfig);
+				}
+				logger.debug("Cloud Config: " + JSON.stringify(cloudConfig));
+				return cloudConfig;
+			} catch (error) {
+				logger.error("Error reading cloud config: %s", error.message);
+				return cloudConfig;
+			};
+		} catch (e) {
+			logger.error("Caught error from cloud config client: %s", e.message);
 			return cloudConfig;
 		}
+	} else {
+		return cloudConfig;
 	}
 }
--- test/unit/utils-test.ts
+++ test/unit/utils-test.ts
@@ -9,212 +9,177 @@ import {
 
 describe('utils', function() {
 
-	describe("#readYaml()", function () {
-		it("should read test yaml without profiles", function (done) {
+	describe("#readYaml()", function() {
+		it("should read test yaml without profiles", async function() {
 			const testProperties = readYaml('./test/fixtures/readYaml/test.yml')
 			assert.deepEqual(testProperties['test.unit.testBool'], true);
 			assert.deepEqual(testProperties['test.unit.testString'], 'testing');
 			assert.deepEqual(testProperties['test.unit.testNumber'], 12345);
-			done();
 		});
 
-		it("should read yaml and parse doc by profiles", function (done) {
+		it("should read yaml and parse doc by profiles", async function() {
 			const testProperties = readYaml('./test/fixtures/readYaml/test-yaml-docs.yml', ['development'])
 			assert.deepEqual(testProperties['test.unit.testBool'], true);
 			assert.deepEqual(testProperties['test.unit.testString'], 'testing again');
 			assert.deepEqual(testProperties['test.unit.testNumber'], 23456);
-			done();
 		});
 
-		it("should read yaml and parse doc by profiles, even with multiple profiles", function (done) {
+		it("should read yaml and parse doc by profiles, even with multiple profiles", async function() {
 			const testProperties = readYaml('./test/fixtures/readYaml/test-yaml-with-profiles.yml', ['env1','env4'])
 			assert.deepEqual(testProperties['urlProperty'], 'http://www.testdomain-shared.com');
 			assert.deepEqual(testProperties['propertyGroup']['groupProperty'], false);
-			done();
 		});
 	});
 
-	describe("#mergeProperties()", function () {
-		it("should merge properties between two files", function () {
-			return new Promise((resolve, reject) => {
-				var obj1 = {
-					'test.unit.testBool': true,
-					'test.unit.testString': 'testing',
-					'test.unit.testNumber': 12345
-				};
-				var obj2 = {
-					'test.unit.testBool': true,
-					'test.unit.testString': 'testing again',
-					'test.unit.testNumber': 12345
-				};
+	describe("#mergeProperties()", function() {
+		it("should merge properties between two files", async function() {
+			var obj1 = {
+				'test.unit.testBool': true,
+				'test.unit.testString': 'testing',
+				'test.unit.testNumber': 12345
+			};
+			var obj2 = {
+				'test.unit.testBool': true,
+				'test.unit.testString': 'testing again',
+				'test.unit.testNumber': 12345
+			};
 
-				var mergedProperties = mergeProperties([obj1, obj2]);
-				assert.deepEqual(mergedProperties['test.unit.testBool'], true);
-				assert.deepEqual(mergedProperties['test.unit.testString'], 'testing again');
-				assert.deepEqual(mergedProperties['test.unit.testNumber'], 12345);
-				resolve();
-			});
+			var mergedProperties = mergeProperties([obj1, obj2]);
+			assert.deepEqual(mergedProperties['test.unit.testBool'], true);
+			assert.deepEqual(mergedProperties['test.unit.testString'], 'testing again');
+			assert.deepEqual(mergedProperties['test.unit.testNumber'], 12345);
 		});
 	});
 
-	describe("#parsePropertiesToObjects()", function () {
-		it("should skip undefined object", function () {
-			return new Promise((resolve, reject) => {
-				let mergedProperties;
-				let configObject = parsePropertiesToObjects(mergedProperties);
-				assert.deepEqual(configObject, {});
-				resolve();
-			});
+	describe("#parsePropertiesToObjects()", function() {
+		it("should skip undefined object", async function() {
+			let mergedProperties;
+			let configObject = parsePropertiesToObjects(mergedProperties);
+			assert.deepEqual(configObject, {});
 		});
-		it("should parse dot-separated properties into JS object", function () {
-			return new Promise((resolve, reject) => {
-				let mergedProperties = {
-					'test.unit.testBool': true,
-					'test.unit.testString': 'testing again',
-					'test.unit.testNumber': 12345
-				};
-				let expectedObject = {
-					test: {
-						unit: {
-							testBool: true,
-							testString: 'testing again',
-							testNumber: 12345
-						}
+		it("should parse dot-separated properties into JS object", async function() {
+			let mergedProperties = {
+				'test.unit.testBool': true,
+				'test.unit.testString': 'testing again',
+				'test.unit.testNumber': 12345
+			};
+			let expectedObject = {
+				test: {
+					unit: {
+						testBool: true,
+						testString: 'testing again',
+						testNumber: 12345
 					}
-				};
-				let configObject = parsePropertiesToObjects(mergedProperties);
-				assert.deepEqual(configObject, expectedObject);
-				resolve();
-			});
+				}
+			};
+			let configObject = parsePropertiesToObjects(mergedProperties);
+			assert.deepEqual(configObject, expectedObject);
 		});
 	});
 
-	describe("#createObjectForProperty()", function () {
-		it("should construct a JS Object given a Boolean property's keys and value", function () {
-			return new Promise((resolve, reject) => {
-				let expectedObjectFromBool = {
-					test: {
-						unit: {
-							testBool: true
-						}
+	describe("#createObjectForProperty()", function() {
+		it("should construct a JS Object given a Boolean property's keys and value", async function() {
+			let expectedObjectFromBool = {
+				test: {
+					unit: {
+						testBool: true
 					}
-				};
-				let boolObject = createObjectForProperty(['test', 'unit', 'testBool'], true);
-				assert.deepEqual(boolObject, expectedObjectFromBool);
-				resolve();
-			});
+				}
+			};
+			let boolObject = createObjectForProperty(['test', 'unit', 'testBool'], true);
+			assert.deepEqual(boolObject, expectedObjectFromBool);
 		});
 
-		it("should construct a JS Object given a String property's keys and value", function () {
-			return new Promise((resolve, reject) => {
-				let expectedObjectFromString = {
-					test: {
-						unit: {
-							testString: 'testing'
-						}
+		it("should construct a JS Object given a String property's keys and value", async function() {
+			let expectedObjectFromString = {
+				test: {
+					unit: {
+						testString: 'testing'
 					}
-				};
-				let stringObject = createObjectForProperty(['test', 'unit', 'testString'], 'testing');
-				assert.deepEqual(stringObject, expectedObjectFromString);
-				resolve();
-			});
+				}
+			};
+			let stringObject = createObjectForProperty(['test', 'unit', 'testString'], 'testing');
+			assert.deepEqual(stringObject, expectedObjectFromString);
 		});
 
-		it("should construct a JS Object given a Number property's keys and value", function () {
-			return new Promise((resolve, reject) => {
-				let expectedObjectFromNumber = {
-					test: {
-						unit: {
-							testNumber: 12345
-						}
+		it("should construct a JS Object given a Number property's keys and value", async function() {
+			let expectedObjectFromNumber = {
+				test: {
+					unit: {
+						testNumber: 12345
 					}
-				};
-				let numberObject = createObjectForProperty(['test', 'unit', 'testNumber'], 12345);
-				assert.deepEqual(numberObject, expectedObjectFromNumber);
-				resolve();
-			});
+				}
+			};
+			let numberObject = createObjectForProperty(['test', 'unit', 'testNumber'], 12345);
+			assert.deepEqual(numberObject, expectedObjectFromNumber);
 		});
 	});
 
 	describe('#shouldUseDocument()', function() {
 
-		it('should not use undefined document', function(done) {
+		it('should not use undefined document', async function() {
 			let doc = undefined;
 			let activeProfiles = ['aProfile'];
 			// @ts-ignore
 			assert.deepEqual(shouldUseDocument(doc, activeProfiles), false);
-			done();
 		});
 
-		it('should not use document that has profiles, but no activeProfiles input',
-				function(done) {
-					let doc = {'profiles': 'aProfile'};
-					let activeProfiles;
-					assert.deepEqual(shouldUseDocument(doc, activeProfiles), false);
-					done();
-				});
+		it('should not use document that has profiles, but no activeProfiles input', async function() {
+			let doc = {'profiles': 'aProfile'};
+			let activeProfiles;
+			assert.deepEqual(shouldUseDocument(doc, activeProfiles), false);
+		});
 
-		it('should use document when doc.profiles is undefined', function(done) {
+		it('should use document when doc.profiles is undefined', async function() {
 			let doc = {};
 			let activeProfiles = ['aProfile'];
 			assert.deepEqual(shouldUseDocument(doc, activeProfiles), true);
-			done();
 		});
 
-		it('should use document when activeProfiles matches a single doc.profiles',
-				function(done) {
-					let doc = {'profiles': 'devEast'};
-					let activeProfiles = ['devEast'];
-					assert.deepEqual(shouldUseDocument(doc, activeProfiles), true);
-					done();
-				});
+		it('should use document when activeProfiles matches a single doc.profiles', async function() {
+			let doc = {'profiles': 'devEast'};
+			let activeProfiles = ['devEast'];
+			assert.deepEqual(shouldUseDocument(doc, activeProfiles), true);
+		});
 
-		it('should use document when one of activeProfiles matches one of doc.profiles',
-				function(done) {
-					let doc = {'profiles': 'devEast,devWest,stagingEast'};
-					let activeProfiles = ['devEast'];
-					assert.deepEqual(shouldUseDocument(doc, activeProfiles), true);
-					activeProfiles = ['devWest'];
-					assert.deepEqual(shouldUseDocument(doc, activeProfiles), true);
-					activeProfiles = ['stagingEast'];
-					assert.deepEqual(shouldUseDocument(doc, activeProfiles), true);
-					done();
-				});
+		it('should use document when one of activeProfiles matches one of doc.profiles', async function() {
+			let doc = {'profiles': 'devEast,devWest,stagingEast'};
+			let activeProfiles = ['devEast'];
+			assert.deepEqual(shouldUseDocument(doc, activeProfiles), true);
+			activeProfiles = ['devWest'];
+			assert.deepEqual(shouldUseDocument(doc, activeProfiles), true);
+			activeProfiles = ['stagingEast'];
+			assert.deepEqual(shouldUseDocument(doc, activeProfiles), true);
+		});
 
-		it('should use document when multiple doc.profiles match active profiles',
-				function(done) {
-					let doc = {'profiles': 'devEast,devWest,stagingEast'};
-					let activeProfiles = ['devEast','devWest'];
-					assert.deepEqual(shouldUseDocument(doc, activeProfiles), true);
-					activeProfiles = ['devWest','stagingEast'];
-					assert.deepEqual(shouldUseDocument(doc, activeProfiles), true);
-					activeProfiles = ['stagingEast','devEast'];
-					assert.deepEqual(shouldUseDocument(doc, activeProfiles), true);
-					done();
-				});
+		it('should use document when multiple doc.profiles match active profiles', async function() {
+			let doc = {'profiles': 'devEast,devWest,stagingEast'};
+			let activeProfiles = ['devEast','devWest'];
+			assert.deepEqual(shouldUseDocument(doc, activeProfiles), true);
+			activeProfiles = ['devWest','stagingEast'];
+			assert.deepEqual(shouldUseDocument(doc, activeProfiles), true);
+			activeProfiles = ['stagingEast','devEast'];
+			assert.deepEqual(shouldUseDocument(doc, activeProfiles), true);
+		});
 
-		it('should NOT use document when not operator used in doc.profiles for active profile',
-				function(done) {
-					let doc = {'profiles': 'devEast,!devWest,stagingEast'};
-					let activeProfiles = ['devEast','devWest'];
-					assert.deepEqual(shouldUseDocument(doc, activeProfiles), false);
-					activeProfiles = ['devWest','stagingEast'];
-					assert.deepEqual(shouldUseDocument(doc, activeProfiles), false);
-					done();
-				});
+		it('should NOT use document when not operator used in doc.profiles for active profile', async function() {
+			let doc = {'profiles': 'devEast,!devWest,stagingEast'};
+			let activeProfiles = ['devEast','devWest'];
+			assert.deepEqual(shouldUseDocument(doc, activeProfiles), false);
+			activeProfiles = ['devWest','stagingEast'];
+			assert.deepEqual(shouldUseDocument(doc, activeProfiles), false);
+		});
 
-		it('should use document when not operator used in doc.profiles for non-active profile',
-				function(done) {
-					let doc = {'profiles': 'devEast,devWest,!stagingEast'};
-					let activeProfiles = ['devEast','devWest'];
-					assert.deepEqual(shouldUseDocument(doc, activeProfiles), true);
-					doc = {'profiles': 'devEast,!devWest,stagingEast'};
-					activeProfiles = ['devEast'];
-					assert.deepEqual(shouldUseDocument(doc, activeProfiles), true);
-					doc = {'profiles': '!devEast,devWest,stagingEast'};
-					activeProfiles = ['devWest','stagingEast'];
-					assert.deepEqual(shouldUseDocument(doc, activeProfiles), true);
-					done();
-				});
+		it('should use document when not operator used in doc.profiles for non-active profile', async function() {
+			let doc = {'profiles': 'devEast,devWest,!stagingEast'};
+			let activeProfiles = ['devEast','devWest'];
+			assert.deepEqual(shouldUseDocument(doc, activeProfiles), true);
+			doc = {'profiles': 'devEast,!devWest,stagingEast'};
+			activeProfiles = ['devEast'];
+			assert.deepEqual(shouldUseDocument(doc, activeProfiles), true);
+			doc = {'profiles': '!devEast,devWest,stagingEast'};
+			activeProfiles = ['devWest','stagingEast'];
+			assert.deepEqual(shouldUseDocument(doc, activeProfiles), true);
+		});
 	});
 });
