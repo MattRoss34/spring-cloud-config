cd81d850f565a1e097006a6ec64487a9e0551fec
Matt Ross <matthewtross@yahoo.com>
2019-06-30 01:16:22 +0300

    Add JOI Schemas, Update Tests, and Cleanup Library (#32)
    * Test cloud config failure
    
    * Types Cleanup
    
    * Use const where appropriate
    
    * Add Joi Schema Validation
    Update usage of Sinon sandbox
    Decache SpringCloudConfig after each test
    Use chai-as-promised in tests
    
    * Bump Version
    
    * Add profiles to readme
    
    * Bump Node Version in Travis Builds

--- test/unit/utils-test.ts
+++ test/unit/utils-test.ts
@@ -6,64 +6,71 @@ import {
 	createObjectForProperty,
 	shouldUseDocument
 } from '../../src/utils';
+import { Document } from '../../src';
 
 describe('utils', function() {
 
 	describe("#readYaml()", function() {
+
 		it("should read test yaml without profiles", async function() {
-			const testProperties = readYaml('./test/fixtures/readYaml/test.yml')
+			const testProperties: Document = readYaml('./test/fixtures/readYaml/test.yml')
 			assert.deepEqual(testProperties['test.unit.testBool'], true);
 			assert.deepEqual(testProperties['test.unit.testString'], 'testing');
 			assert.deepEqual(testProperties['test.unit.testNumber'], 12345);
 		});
 
 		it("should read yaml and parse doc by profiles", async function() {
-			const testProperties = readYaml('./test/fixtures/readYaml/test-yaml-docs.yml', ['development'])
+			const testProperties: Document = readYaml('./test/fixtures/readYaml/test-yaml-docs.yml', ['development'])
 			assert.deepEqual(testProperties['test.unit.testBool'], true);
 			assert.deepEqual(testProperties['test.unit.testString'], 'testing again');
 			assert.deepEqual(testProperties['test.unit.testNumber'], 23456);
 		});
 
 		it("should read yaml and parse doc by profiles, even with multiple profiles", async function() {
-			const testProperties = readYaml('./test/fixtures/readYaml/test-yaml-with-profiles.yml', ['env1','env4'])
+			const testProperties: Document = readYaml('./test/fixtures/readYaml/test-yaml-with-profiles.yml', ['env1','env4'])
 			assert.deepEqual(testProperties['urlProperty'], 'http://www.testdomain-shared.com');
 			assert.deepEqual(testProperties['propertyGroup']['groupProperty'], false);
 		});
+
 	});
 
 	describe("#mergeProperties()", function() {
+
 		it("should merge properties between two files", async function() {
-			var obj1 = {
+			var obj1: object = {
 				'test.unit.testBool': true,
 				'test.unit.testString': 'testing',
 				'test.unit.testNumber': 12345
 			};
-			var obj2 = {
+			var obj2: object = {
 				'test.unit.testBool': true,
 				'test.unit.testString': 'testing again',
 				'test.unit.testNumber': 12345
 			};
 
-			var mergedProperties = mergeProperties([obj1, obj2]);
+			var mergedProperties: object = mergeProperties([obj1, obj2]);
 			assert.deepEqual(mergedProperties['test.unit.testBool'], true);
 			assert.deepEqual(mergedProperties['test.unit.testString'], 'testing again');
 			assert.deepEqual(mergedProperties['test.unit.testNumber'], 12345);
 		});
+
 	});
 
 	describe("#parsePropertiesToObjects()", function() {
+
 		it("should skip undefined object", async function() {
-			let mergedProperties;
-			let configObject = parsePropertiesToObjects(mergedProperties);
+			const mergedProperties: object | undefined = undefined;
+			const configObject: object = parsePropertiesToObjects(mergedProperties);
 			assert.deepEqual(configObject, {});
 		});
+
 		it("should parse dot-separated properties into JS object", async function() {
-			let mergedProperties = {
+			const mergedProperties: object = {
 				'test.unit.testBool': true,
 				'test.unit.testString': 'testing again',
 				'test.unit.testNumber': 12345
 			};
-			let expectedObject = {
+			const expectedObject: object = {
 				test: {
 					unit: {
 						testBool: true,
@@ -72,79 +79,82 @@ describe('utils', function() {
 					}
 				}
 			};
-			let configObject = parsePropertiesToObjects(mergedProperties);
+			const configObject: object = parsePropertiesToObjects(mergedProperties);
 			assert.deepEqual(configObject, expectedObject);
 		});
+
 	});
 
 	describe("#createObjectForProperty()", function() {
+
 		it("should construct a JS Object given a Boolean property's keys and value", async function() {
-			let expectedObjectFromBool = {
+			const expectedObjectFromBool: object = {
 				test: {
 					unit: {
 						testBool: true
 					}
 				}
 			};
-			let boolObject = createObjectForProperty(['test', 'unit', 'testBool'], true);
+			const boolObject: object = createObjectForProperty(['test', 'unit', 'testBool'], true);
 			assert.deepEqual(boolObject, expectedObjectFromBool);
 		});
 
 		it("should construct a JS Object given a String property's keys and value", async function() {
-			let expectedObjectFromString = {
+			const expectedObjectFromString: object = {
 				test: {
 					unit: {
 						testString: 'testing'
 					}
 				}
 			};
-			let stringObject = createObjectForProperty(['test', 'unit', 'testString'], 'testing');
+			const stringObject: object = createObjectForProperty(['test', 'unit', 'testString'], 'testing');
 			assert.deepEqual(stringObject, expectedObjectFromString);
 		});
 
 		it("should construct a JS Object given a Number property's keys and value", async function() {
-			let expectedObjectFromNumber = {
+			const expectedObjectFromNumber: object = {
 				test: {
 					unit: {
 						testNumber: 12345
 					}
 				}
 			};
-			let numberObject = createObjectForProperty(['test', 'unit', 'testNumber'], 12345);
+			const numberObject: object = createObjectForProperty(['test', 'unit', 'testNumber'], 12345);
 			assert.deepEqual(numberObject, expectedObjectFromNumber);
 		});
+
 	});
 
 	describe('#shouldUseDocument()', function() {
 
 		it('should not use undefined document', async function() {
-			let doc = undefined;
-			let activeProfiles = ['aProfile'];
+			const doc: Document | undefined = undefined;
+			const activeProfiles: string[] = ['aProfile'];
 			// @ts-ignore
 			assert.deepEqual(shouldUseDocument(doc, activeProfiles), false);
 		});
 
 		it('should not use document that has profiles, but no activeProfiles input', async function() {
-			let doc = {'profiles': 'aProfile'};
-			let activeProfiles;
+			const doc: Document = {'profiles': 'aProfile'};
+			const activeProfiles: string[] | undefined = undefined;
 			assert.deepEqual(shouldUseDocument(doc, activeProfiles), false);
 		});
 
 		it('should use document when doc.profiles is undefined', async function() {
-			let doc = {};
-			let activeProfiles = ['aProfile'];
+			const doc: Document = {};
+			const activeProfiles: string[] = ['aProfile'];
 			assert.deepEqual(shouldUseDocument(doc, activeProfiles), true);
 		});
 
 		it('should use document when activeProfiles matches a single doc.profiles', async function() {
-			let doc = {'profiles': 'devEast'};
-			let activeProfiles = ['devEast'];
+			const doc: Document = {'profiles': 'devEast'};
+			const activeProfiles: string[] = ['devEast'];
 			assert.deepEqual(shouldUseDocument(doc, activeProfiles), true);
 		});
 
 		it('should use document when one of activeProfiles matches one of doc.profiles', async function() {
-			let doc = {'profiles': 'devEast,devWest,stagingEast'};
-			let activeProfiles = ['devEast'];
+			const doc: Document = {'profiles': 'devEast,devWest,stagingEast'};
+			let activeProfiles: string[] = ['devEast'];
 			assert.deepEqual(shouldUseDocument(doc, activeProfiles), true);
 			activeProfiles = ['devWest'];
 			assert.deepEqual(shouldUseDocument(doc, activeProfiles), true);
@@ -153,8 +163,8 @@ describe('utils', function() {
 		});
 
 		it('should use document when multiple doc.profiles match active profiles', async function() {
-			let doc = {'profiles': 'devEast,devWest,stagingEast'};
-			let activeProfiles = ['devEast','devWest'];
+			const doc: Document = {'profiles': 'devEast,devWest,stagingEast'};
+			let activeProfiles: string[] = ['devEast','devWest'];
 			assert.deepEqual(shouldUseDocument(doc, activeProfiles), true);
 			activeProfiles = ['devWest','stagingEast'];
 			assert.deepEqual(shouldUseDocument(doc, activeProfiles), true);
@@ -163,16 +173,16 @@ describe('utils', function() {
 		});
 
 		it('should NOT use document when not operator used in doc.profiles for active profile', async function() {
-			let doc = {'profiles': 'devEast,!devWest,stagingEast'};
-			let activeProfiles = ['devEast','devWest'];
+			const doc: Document = {'profiles': 'devEast,!devWest,stagingEast'};
+			let activeProfiles: string[] = ['devEast','devWest'];
 			assert.deepEqual(shouldUseDocument(doc, activeProfiles), false);
 			activeProfiles = ['devWest','stagingEast'];
 			assert.deepEqual(shouldUseDocument(doc, activeProfiles), false);
 		});
 
 		it('should use document when not operator used in doc.profiles for non-active profile', async function() {
-			let doc = {'profiles': 'devEast,devWest,!stagingEast'};
-			let activeProfiles = ['devEast','devWest'];
+			let doc: Document = {'profiles': 'devEast,devWest,!stagingEast'};
+			let activeProfiles: string[] = ['devEast','devWest'];
 			assert.deepEqual(shouldUseDocument(doc, activeProfiles), true);
 			doc = {'profiles': 'devEast,!devWest,stagingEast'};
 			activeProfiles = ['devEast'];
@@ -181,5 +191,7 @@ describe('utils', function() {
 			activeProfiles = ['devWest','stagingEast'];
 			assert.deepEqual(shouldUseDocument(doc, activeProfiles), true);
 		});
+
 	});
+
 });
